d07a2677bd30efde599c9192cb68c09ad002b68f
Carlos Jair Nieto <149533058+CJ-Nieto@users.noreply.github.com>
2024-07-01 20:05:07 -0600

    Add files via upload

---
+++ modules/instances/instances.tf
@@ -0,0 +1,89 @@
+
+resource "google_compute_instance" "tf-instance-1" {
+  name         = "tf-instance-1"
+  machine_type = "e2-standard-2"
+  zone         = "us-east1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "tf-vpc-726251"
+     subnetwork = "subnet-01"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_instance" "tf-instance-2" {
+  name         = "tf-instance-2"
+  machine_type = "e2-standard-2"
+  zone         = "us-east1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "tf-vpc-726251"
+     subnetwork = "subnet-02"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+resource "google_compute_instance" "tf-instance-608786" {
+  name         = "tf-instance-608786"
+  machine_type = "e2-standard-2"
+  zone         = "us-east1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "tf-vpc-726251"
+     subnetwork = "subnet-02"
+  }
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+    EOT
+  allow_stopping_for_update = true
+}
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 6.0.0"
+
+    project_id   = "qwiklabs-gcp-00-f2a794885742"
+    network_name = "tf-vpc-726251"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-east1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-east1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "Please like share & subscribe to quicklab"
+        },
+    ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "region" {
+ default = "us-east1"
+}
+
+variable "zone" {
+ default = "us-east1-c"
+}
+
+variable "project_id" {
+ default = "qwiklabs-gcp-00-f2a794885742"
+}
